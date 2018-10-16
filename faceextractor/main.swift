12c21b9c03949370f3be193bd24982b22812917e
Dmitriy Osipov <dosipov@azbukavkusa.ru>
2018-10-16 14:28:34 +0300

    Initial Commit

---
+++ faceextractor/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  faceextractor
+//
+//  Created by Dmitriy Osipov on 16/10/2018.
+//  Copyright © 2018 Dmitriy Osipov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
